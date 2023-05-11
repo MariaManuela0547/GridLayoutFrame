40fa15ef8ff71e3963025c356144e3cb4c409b42
MariaManuela0547 <124294920+MariaManuela0547@users.noreply.github.com>
2023-05-11 15:15:53 -0400

    Add files via upload

---
+++ GridLayoutDemo.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class GridLayoutDemo {
+
+    public static void main(String[] args) {
+       
+        GridLayoutFrame gridLayoutFrame = new GridLayoutFrame();
+        gridLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gridLayoutFrame.setSize(300,200); // configura o tamanho do frame
+        gridLayoutFrame .setVisible(true); // exibe o frame
+                
+    } // fim de main
+    
+} // fim da classe GridLayoutDemo
